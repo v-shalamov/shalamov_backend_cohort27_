9941c30ce4c865e670caff0ddae1cdcbbae5e82e
Vadim Shalamov <reetvvy@gmail.com>
2024-06-11 16:01:58 +0300

    getting started with the EventsRepositoryFileImplTest and EventsServiceImplTest classes

--- src/Task_02/EventsTests/test/repositories/impl/EventsRepositoryFileImplTest.java
+++ src/Task_02/EventsTests/test/repositories/impl/EventsRepositoryFileImplTest.java
@@ -1,4 +1,8 @@
 package Task_02.EventsTests.test.repositories.impl;
 
+@DisplayName("EventsRepositoryImplTest is works ...")
+@DisplayNameGeneration(value = DisplayNameGeneration.ReplaceUnderscores.class)
 public class EventsRepositoryFileImplTest {
+
+    private static final String TEMP_EVENTS_FILE_NAME = "events_test.txt";
 }
--- src/Task_02/EventsTests/test/services/impl/EventsServiceImplTest.java
+++ src/Task_02/EventsTests/test/services/impl/EventsServiceImplTest.java
@@ -1,4 +1,7 @@
 package Task_02.EventsTests.test.services.impl;
 
+@DisplayName("EventsServiceImplTest is works ...")
+@DisplayNameGeneration(value = DisplayNameGeneration.ReplaceUnderscores.class)
 public class EventsServiceImplTest {
+
 }
